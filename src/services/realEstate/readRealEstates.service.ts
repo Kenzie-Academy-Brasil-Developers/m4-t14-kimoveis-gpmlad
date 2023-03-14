bb9c70bf9a9796cb6116da82e6cf4729fd54be7b
Giovanni Perotto <giovanniperotto@hotmail.com>
2023-03-14 10:57:20 -0300

    fix: realEstate

--- src/services/realEstate/readRealEstates.service.ts
+++ src/services/realEstate/readRealEstates.service.ts
@@ -4,22 +4,16 @@ import { RealEstate } from "../../entities"
 import { iRealEstates } from "../../interfaces/realEstate.interfaces"
 import { allRealEstate } from "../../schemas/realEstate.schemas"
 
-const readRealEstateService = async():Promise<iRealEstates> => {
+const readRealEstateService = async():Promise<RealEstate[]> => {
 
   const realEstateRepository: Repository<RealEstate> = AppDataSource.getRepository(RealEstate)
 
   const findRealEstates: Array<RealEstate> = await realEstateRepository.find({
     relations:{
-      address: true,
-      category: true
+      address: true
     }
   })
-
-  console.log(findRealEstates)
-
-  const realEstates = allRealEstate.parse(findRealEstates)
-
-  return realEstates
+  return findRealEstates
 }
 
 export default readRealEstateService
--- src/services/schedules/createSchedule.service.ts
+++ src/services/schedules/createSchedule.service.ts
@@ -1,6 +1,7 @@
 import { Repository } from "typeorm"
 import { AppDataSource } from "../../data-source"
 import { RealEstate, Schedule, User } from "../../entities"
+import { AppError } from "../../errors"
 import { iScheduleRequest } from "../../interfaces/schedules.interfaces"
 
 const createScheduleService = async(scheduleData: iScheduleRequest, userId: number):Promise<any> => {
@@ -20,11 +21,32 @@ const createScheduleService = async(scheduleData: iScheduleRequest, userId: numb
   const findRealEstate = await realEstateRepository.findOne({
     where:{
       id: scheduleData.realEstateId ? scheduleData.realEstateId : 0
+    },
+    relations:{
+      schedule: true
     }
   })
+
+  if(!findRealEstate){
+    throw new AppError("RealEstate not found",404)
+  }
+
   const newDate = new Date(scheduleData.date)
   newDate.setHours(+scheduleData.hour.split(":")[0],+scheduleData.hour.split(":")[1])
- 
+
+  if(+scheduleData.hour.split(":")[0] < 8 || +scheduleData.hour.split(":")[0]> 18){
+    throw new AppError("Invalid hour, available times are 8AM to 18PM",400)
+  }
+
+  if(newDate.getDay() == 0 || newDate.getDay() == 6){
+    throw new AppError("Invalid date, work days are monday to friday",400)
+  }
+
+  //console.log(findRealEstate.schedule[0].hour && findRealEstate.schedule[0].date)
+  //if(findRealEstate.schedule[0].hour == newDate.toString() && findRealEstate.schedule[0].date == `${newDate.getHours()}:${newDate.getMinutes()}`){
+  //  throw new AppError("Schedule to this real estate at this date and time already exists",409)
+  //}
+
   const newSchedule = {
     date: newDate.toString(),
     hour: `${newDate.getHours()}:${newDate.getMinutes()}`,
@@ -35,8 +57,7 @@ const createScheduleService = async(scheduleData: iScheduleRequest, userId: numb
   const updatedSchedule = scheduleRepository.create(newSchedule)
   await scheduleRepository.save(updatedSchedule)
 
-  console.log(updatedSchedule)
-  return updatedSchedule 
+  return {message: "Schedule created"}
 }
 
 export default createScheduleService
